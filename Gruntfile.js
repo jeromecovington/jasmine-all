a18e9332e53692091c2945fb6f5a97ebea86dcfc
jeromecovington <jeromecovington@gmail.com>
2014-06-13 10:52:42 -0400

    Base Gruntfile and uglified dist.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,21 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-            separator: ';'
-            },
-            dist: {
-                src: [
-                    'lib/jasmine-css.js',
-                    'lib/jasmine.js',
-                    'lib/jasmine-html.js',
-                    'lib/boot.js'
-                ],
-                dest: 'dist/jasmine-all.js'
+        uglify: {
+            my_target: {
+                files: {
+                    'dist/jasmine-all.js': [
+                        'lib/jasmine-css.js',
+                        'lib/jasmine.js',
+                        'lib/jasmine-html.js',
+                        'lib/boot.js'
+                    ]
+                }
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['uglify']);
 };
